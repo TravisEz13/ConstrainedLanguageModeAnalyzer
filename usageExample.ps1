31df5890dda22818b444e7ef2741f1db5d9e1b09
travis plunk <github@ez13.net>
2023-02-17 07:17:21 -0800

    Fix usage example

--- usageExample.ps1
+++ usageExample.ps1
@@ -1,9 +1,11 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-if ((get-module -ListAvailable PSScriptAnalyzer -ErrorAction Stop)) {
+if (!(get-module -ListAvailable PSScriptAnalyzer -ErrorAction Stop)) {
     Install-Module -Name PSScriptAnalyzer -Force
 }
+
+Import-Module PSScriptAnalyzer
 
 Invoke-ScriptAnalyzer -Path "$PSScriptRoot\testFiles\*" `
     -CustomizedRulePath (Resolve-Path $PSScriptRoot\ConstrainedLanguageModeAnalyzer/ConstrainedLanguageModeAnalyzer.psm1) `
